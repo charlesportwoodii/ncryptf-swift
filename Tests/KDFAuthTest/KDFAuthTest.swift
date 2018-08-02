063243fde4fa3d95233694c30e4af61d4eab4801
Charles R. Portwood II <charlesportwoodii@erianna.com>
2018-08-02 07:13:29 -0500

    Updating tests so at least it runs

--- Tests/KDFAuthTest/KDFAuthTest.swift
+++ Tests/KDFAuthTest/KDFAuthTest.swift
@@ -19,13 +19,12 @@ class KDFAuthTest: XCTestCase {
         ("testTokenIsNotExpired", testTokenIsNotExpired)
     ]
 
-    let kdfAuth = KDFAuth()
-    let tokens = kdfAuth.createToken(
+    let token = KDFAuth.createToken(
         accessToken: "x2gMeJ5Np0CcKpZav+i9iiXeQBtaYMQ/yeEtcOgY3J",
         refreshToken: "LRSEe5zHb1aq20Hr9te2sQF8sLReSkO8bS1eD/9LDM8",
         ikm: "f2mTaH9vkZZQyF7SxVeXDlOSDbVwjUzhdXv2T/YYO8k=",
-        salt: "waWBMawHD1zpAFRcX7e45L1aqsA3mEeSOwXqq4l1i3I=",
-        expiresAt: Date.timeIntervalSince1970 + (60 * 60 * 60)
+        signature: "waWBMawHD1zpAFRcX7e45L1aqsA3mEeSOwXqq4l1i3I=",
+        expiresAt: Date().timeIntervalSince1970 + (60 * 60 * 60)
     )
 
     override func setUp() {
